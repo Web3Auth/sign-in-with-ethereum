023337cc56db7cd835a858413e67241555cd7755
hieu-w <hieu@tor.us>
2025-02-24 01:05:01 +0700

    Fix CI

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
 const { EnvironmentPlugin } = require("webpack");
 
 require("dotenv").config({ path: ".env" });
 
 exports.baseConfig = {
-  plugins: [new EnvironmentPlugin(["VITE_APP_INFURA_PROJECT_KEY"])],
+  plugins: [
+    new EnvironmentPlugin({
+      VITE_APP_INFURA_PROJECT_KEY: "",
+    }),
+  ],
 };
