828f05b62f4a0cbfe8ccacfde819efbfd0f6a1e6
Chaitanya Potti <chaitanya.potti@gmail.com>
2025-03-24 23:38:04 +0800

    use og webpack syntax for building

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,5 @@ const { EnvironmentPlugin } = require("webpack");
 require("dotenv").config({ path: ".env" });
 
 exports.baseConfig = {
-  plugins: [
-    new EnvironmentPlugin({
-      VITE_APP_INFURA_PROJECT_KEY: "",
-    }),
-  ],
+  plugins: [new EnvironmentPlugin("VITE_APP_INFURA_PROJECT_KEY")],
 };
